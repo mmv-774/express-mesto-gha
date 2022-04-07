0b3e508b5a20526b31f446500e2643a53555bef7
MIkhail <mmv-774@yandex.ru>
2022-04-07 10:58:04 +0500

    создать схемы и модели

---
+++ models/card.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  like: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', schema);
