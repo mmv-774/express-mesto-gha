e6c2d94fcd7953a18a772fc0ef03ca4f237ea8b2
MIkhail <mmv-774@yandex.ru>
2022-04-10 21:43:19 +0500

    обработка ошибок в контроллере пользователей

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const { DocumentNotFoundError, ValidationError } = require('mongoose').Error;
 const { ObjectId } = require('mongoose').Types;
 const HttpError = require('../errors/HttpError');
 const User = require('../models/user');
@@ -11,14 +12,15 @@ module.exports.getUsers = (req, res, next) => {
 module.exports.getUserById = (req, res, next) => {
   if (ObjectId.isValid(req.params.userId)) {
     User.findById(req.params.userId)
-      .then((user) => {
-        if (!user) {
+      .orFail()
+      .then((user) => res.send(user))
+      .catch((error) => {
+        if (error instanceof DocumentNotFoundError) {
           next(HttpError.notFound('Пользователь по указанному id не найден'));
-          return;
+        } else {
+          next(HttpError.internal());
         }
-        res.send(user);
-      })
-      .catch(() => next(HttpError.internal()));
+      });
   } else {
     next(HttpError.badRequest('Переданы некорректные данные для получения пользователя'));
   }
@@ -26,48 +28,49 @@ module.exports.getUserById = (req, res, next) => {
 
 module.exports.createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
-  const model = new User({
-    name,
-    about,
-    avatar,
-  });
-  model
-    .validate()
-    .then(() => {
-      User.create(model)
-        .then((user) => res.send(user))
-        .catch(() => next(HttpError.internal()));
-    })
-    .catch(() => {
-      next(HttpError.badRequest('Переданы некорректные данные при создании пользователя'));
+  User.create({ name, about, avatar })
+    .then((user) => res.send(user))
+    .catch((error) => {
+      if (error instanceof ValidationError) {
+        next(HttpError.badRequest('Переданы некорректные данные при создании пользователя'));
+      } else {
+        next(HttpError.internal());
+      }
     });
 };
 
 module.exports.patchUserBio = (req, res, next) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .orFail(HttpError.notFound('Пользователь по указанному id не найден'))
+    .orFail()
     .then((user) => res.send(user))
-    .catch((err) => next(HttpError.badRequest('Переданы некорректные данные при обновлении профиля')));
+    .catch((error) => {
+      if (error instanceof DocumentNotFoundError) {
+        next(HttpError.notFound('Пользователь по указанному id не найден'));
+        return;
+      }
+      if (error instanceof ValidationError) {
+        next(HttpError.badRequest('Переданы некорректные данные при обновлении профиля'));
+        return;
+      }
+      next(HttpError.internal());
+    });
 };
 
 module.exports.patchUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  const model = new User({ avatar });
-  model
-    .validate({ validateModifiedOnly: true })
-    .then(() => {
-      User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-        .then((user) => {
-          if (!user) {
-            next(HttpError.notFound('Пользователь по указанному id не найден'));
-            return;
-          }
-          res.send(user);
-        })
-        .catch(() => next(HttpError.internal()));
-    })
-    .catch(() => {
-      next(HttpError.badRequest('Переданы некорректные данные при обновлении аватара'));
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .orFail()
+    .then((user) => res.send(user))
+    .catch((error) => {
+      if (error instanceof DocumentNotFoundError) {
+        next(HttpError.notFound('Пользователь по указанному id не найден'));
+        return;
+      }
+      if (error instanceof ValidationError) {
+        next(HttpError.badRequest('Переданы некорректные данные при обновлении аватара'));
+        return;
+      }
+      next(HttpError.internal());
     });
 };
