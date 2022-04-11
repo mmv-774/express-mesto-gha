78986e23a852fd9797a4a3fcacf350bdf801c430
MIkhail <mmv-774@yandex.ru>
2022-04-11 19:57:35 +0500

    обработка ошибок в контроллере карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,88 +1,63 @@
-const { ObjectId } = require('mongoose').Types;
+const { DocumentNotFoundError, ValidationError, CastError } = require('mongoose').Error;
 const HttpError = require('../errors/HttpError');
 const Card = require('../models/card');
+
+const cardQueryErrorHandler = (error, next, messages = {}) => {
+  if (error instanceof DocumentNotFoundError) {
+    next(HttpError.notFound(messages.documentNotFound || 'Карточка с указанным id не найдена'));
+    return;
+  }
+  if (error instanceof ValidationError || error instanceof CastError) {
+    next(HttpError.badRequest(messages.validation || 'Переданы некорректные данные'));
+    return;
+  }
+  next(HttpError.internal(messages.internal));
+};
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => res.send({ cards }))
-    .catch(() => next(HttpError.internal()));
+    .catch((error) => cardQueryErrorHandler(error, next));
 };
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const model = new Card({
-    name,
-    link,
-    owner: req.user._id,
-  });
-  model
-    .validate()
-    .then(() => {
-      Card.create(model)
-        .then((card) => {
-          card.populate('owner').then((cardWithOwner) => {
-            res.send(cardWithOwner);
-          }).catch(() => next(HttpError.internal()));
-        })
-        .catch(() => next(HttpError.internal()));
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => {
+      card.populate('owner').then((cardWithOwner) => {
+        res.send(cardWithOwner);
+      }).catch((error) => cardQueryErrorHandler(error, next));
     })
-    .catch(() => {
-      next(HttpError.badRequest('Переданы некорректные данные при создании карточки'));
-    });
+    .catch((error) => cardQueryErrorHandler(error, next, { validation: 'Переданы некорректные данные при создании карточки' }));
 };
 
 module.exports.deleteCardById = (req, res, next) => {
-  if (ObjectId.isValid(req.params.cardId)) {
-    Card.findByIdAndRemove(req.params.cardId)
-      .populate(['owner', 'likes'])
-      .then((card) => {
-        if (!card) {
-          next(HttpError.notFound(' Карточка с указанным id не найдена'));
-          return;
-        }
-        res.send(card);
-      })
-      .catch(() => next(HttpError.internal()));
-  } else {
-    next(HttpError.badRequest('Переданы некорректные данные при удаления карточки'));
-  }
+  Card.findByIdAndRemove(req.params.cardId)
+    .orFail()
+    .populate(['owner', 'likes'])
+    .then((card) => res.send(card))
+    .catch((error) => cardQueryErrorHandler(error, next, { validation: 'Переданы некорректные данные при удаления карточки' }));
 };
 
 module.exports.likeCard = (req, res, next) => {
-  if (ObjectId.isValid(req.user._id) && ObjectId.isValid(req.params.cardId)) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    ).populate(['owner', 'likes'])
-      .then((card) => {
-        if (!card) {
-          next(HttpError.notFound(' Карточка с указанным id не найдена'));
-          return;
-        }
-        res.send(card);
-      }).catch(() => next(HttpError.internal()));
-  } else {
-    next(HttpError.badRequest('Переданы некорректные данные для постановки/снятия лайка'));
-  }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  ).orFail()
+    .populate(['owner', 'likes'])
+    .then((card) => res.send(card))
+    .catch((error) => cardQueryErrorHandler(error, next, { validation: 'Переданы некорректные данные для постановки/снятия лайка' }));
 };
 
 module.exports.dislikeCard = (req, res, next) => {
-  if (ObjectId.isValid(req.user._id) && ObjectId.isValid(req.params.cardId)) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-    ).populate(['owner', 'likes'])
-      .then((card) => {
-        if (!card) {
-          next(HttpError.notFound(' Карточка с указанным id не найдена'));
-          return;
-        }
-        res.send(card);
-      }).catch(() => next(HttpError.internal()));
-  } else {
-    next(HttpError.badRequest('Переданы некорректные данные для постановки/снятии лайка'));
-  }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  ).orFail()
+    .populate(['owner', 'likes'])
+    .then((card) => res.send(card))
+    .catch((error) => cardQueryErrorHandler(error, next, { validation: 'Переданы некорректные данные для постановки/снятия лайка' }));
 };
